0b3109ba227f94d833618ad5f5d22b2bcf1f8eaf
MIKIPC\ferna <fernandezmikael@hotmail.com>
2023-03-13 20:04:50 +0100

    Final Code

--- index.js
+++ index.js
@@ -1,51 +1,130 @@
+/*
+Nom et Prenom : Fernandez Mikael
+Nom du fichier : index.js
+Date : 28.02.2021
+Description : Le vue.js du projet gérer un panier d'achat et un système de VIP pour un site de vente de shorts en ligne
+*/
+
+const PRODUCT_MESSAGES = {
+   OUT_OF_STOCK: "Nous sommes désolés, mais ce produit a rencontré un succès tel que les stocks ont fondu plus rapidement qu'une glace en plein soleil.",
+    VIP_MESSAGE: "Vous avez atteint le seuil de 200€ d'achat, vous êtes devenu VIP",
+    ACHAT: "Votre achat a été annulé avec succès ! Vous pouvez maintenant vous reposer sur vos lauriers en sachant que votre carte de crédit est toujours en sécurité. 🛡️😴",
+};
+const VIP_MINIMUM = 200;
 let app = new Vue({
-  el: '#app',
-  data: { 
-      currentIndex: 0,
-      figures: [ 
-          { 
-            key: 1, 
-            name: "Jedi", 
-            image: "img/jedi.jpg", 
-            description: "Guerrier philosophe doté de super-pouvoirs", 
-            style: {
-              backgroundColor: "lightgrey",
-              color: "black"
+    el: '#app',
+    data: {
+        products: [
+            {
+                key: 1,
+                name: "Short Noir",
+                image: "img/blackShort.png",
+                description:"Un joli short noir pour l'été",
+                stock: 4,
+                price: 50
+            },
+            {
+                key: 2,
+                name: "Short bleu",
+                image: "img/blueShort.png",
+                description:"Un joli short bleu pour l'été",
+                stock: 5,
+                price: 40
+            },
+            {
+                key: 3,
+                name: "Short rouge",
+                image: "img/redShort.png",
+                description:"Un joli short rouge pour l'été",
+                stock: 6,
+                price: 70
+            },
+            {
+                key: 4,
+                name: "Short vert",
+                image: "img/greenShort.png",
+                description:"Un joli short vert pour l'été",
+                stock: 5,
+                price: 80
             }
-          }, 
-          { key: 2, 
-            name: "Wookiee", 
-            image: "img/wookiee.jpg", 
-            description: "Grand être humanoïde possédant une épaisse fourrure", 
-            style: {
-              backgroundColor: "#56b0f0",
-              color: "white"
+        ],
+        productAvailability: [
+            {
+                key: 1,
+                message: "Disponible",
+                size: 2,
+                style: "color: green"
+            },
+            {
+                key: 2,
+                message: "Presque épuisé",
+                size: 0,
+                style: "color: orange"
             }
-          }, 
-          { key: 3, 
-            name: "Droide", 
-            image: "img/droide.jpg", 
-            description: "Robot capable d'exécuter des tâches complexes",
-            style: {
-              backgroundColor: "#fcc75b",
-              color: "white"
+        ],
+        cartQuantities: {
+        },
+        currentIndex: 0,
+        vip: false,
+        totalPrice: 0
+    },
+    computed: {
+        selectedProduct: function() {
+            return this.products[this.currentIndex];
+        },
+    },
+    methods: {
+        selectProduct: function(product) {
+            this.currentIndex = this.products.indexOf(product);
+        },
+        addToCart: function() {
+
+            // check si le produit est disponible
+            if (this.selectedProduct.stock === 0) {
+                alert(PRODUCT_MESSAGES.OUT_OF_STOCK);
+                return;
             }
-          }
-      ]
-  },
-  methods: { 
+            // vérifie si le produit est déjà dans le panier
+            if (this.cartQuantities[this.selectedProduct.key]) {
+                this.cartQuantities[this.selectedProduct.key]++;
+            } else {
+                this.cartQuantities[this.selectedProduct.key] = 1;
+            }
 
-      /* Fonction appelée lorsqu'on survole une zone */
-      mouseover(index) {
-          
-          /* Affichage de l'index dans la console */
-          console.log(index)
+            // decrement stock
+            this.selectedProduct.stock--;
 
-          /* Mise en évidence de la zone sélectionnée => obsolete => voir CSS
-          event.target.style.backgroundColor = 'red'; */
+            this.calculateCartTotalPrice();
+            // check le status VIP
+            if (this.totalPrice >= VIP_MINIMUM && !this.vip) {
+                this.vip = true;
+                alert(PRODUCT_MESSAGES.VIP_MESSAGE);
+            }
+        },
+        clearCart: function()
+        {
+            // reset le stock est le panier
+            for (let key in this.cartQuantities) {
+                this.products[key - 1].stock += this.cartQuantities[key];
 
-          /* Changement d'index, afin que l'image change */
-          this.currentIndex = index;
+            }
+            //reset cartQuantities
+            this.cartQuantities = {};
+
+            // reset le status VIP
+            this.vip = false;
+            this.calculateCartTotalPrice();
+        },
+        //calcule le prix total du panier
+        calculateCartTotalPrice: function() {
+            this.totalPrice = 0;
+            for (let key in this.cartQuantities) {
+                this.totalPrice += this.products[key - 1].price * this.cartQuantities[key];
+            }
+        },
+        //achat
+        buy: function () {
+            alert(PRODUCT_MESSAGES.ACHAT);
+        }
     }
-  }
-})
+});
